dfd20f8e288d66d41521c1c4933d2dd1ef285948
dellis2cs <aprules68@yahoo.com>
2024-09-14 19:57:39 -0700

    added current location support

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let api_Key = "FKQLL6TEB82Y887MUA668Y7PX";
+let weather_API_KEY = "FKQLL6TEB82Y887MUA668Y7PX";
+let location_API_KEY = "pk.f5a5101a2f3e6ba85ff301ba7e0b5ce5";
 const temp_H1 = document.querySelector(".temp");
 const search = document.getElementById("search");
 const time = document.querySelector(".time");
@@ -6,11 +7,33 @@ const condition = document.querySelector(".weatherCond");
 const feels = document.querySelector(".feels");
 const location_H1 = document.querySelector(".location");
 const utilities = document.querySelector(".utilities");
+const currLocBtn = document.querySelector(".currentLocationBtn");
 
 function checkKeyPressed(event) {
   if (event.keyCode == "13") {
     fetchWeatherData(search.value);
   }
+}
+
+//Fetches the coordinates of your current location
+function success(pos) {
+  const crd = pos.coords;
+  const lat = crd.latitude;
+  const lon = crd.longitude;
+  getCurrentCity(lat, lon);
+}
+
+currLocBtn.addEventListener("click", function () {
+  navigator.geolocation.getCurrentPosition(success);
+});
+
+async function getCurrentCity(lat, lon) {
+  const response = await fetch(
+    `https://us1.locationiq.com/v1/reverse?key=${location_API_KEY}&lat=${lat}&lon=${lon}&format=json`
+  );
+  const locationData = await response.json();
+  const city = locationData.address.city;
+  fetchWeatherData(city);
 }
 
 // Utility card creation function
@@ -39,7 +62,7 @@ async function fetchWeatherData(location) {
   try {
     //retrieve the weather data for the given location
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${api_Key} `
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${weather_API_KEY} `
     );
 
     //once we retrieve the weather data use json() to read it
